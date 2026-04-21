c1e2135328171dbb7b52212b5269ffd74bb6c3c5
MayrhoferKillian <113448701+MayrhoferKillian@users.noreply.github.com>
2026-04-21 14:43:05 +0200

    Level erweitert + Teleporter

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class teleport : Monobehaviour
+public class Teleport : MonoBehaviour
 {
-	public Transform Player, Destination;
-	public GameObject playerg;
-	
-	void OnTriggerEnter(Collider other){
-		if(other.CompareTag("Player")){
-			playerg.SetActivate(false);
-			player.position = destination.position;
-			playerg.SetActivate(true);
-		}
-	}
+    public Transform destination; 
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false); 
+            other.transform.position = destination.position; 
+            other.gameObject.SetActive(true); 
+        }
+    }
 }
