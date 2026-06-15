b238c79ae0123f1abac5510662fa954980a373c6
Riegler David <david.riegler@htlwy.at>
2026-06-15 18:56:05 +0200

    Shotgun merged w main

--- Assets/Shotgun.cs
+++ Assets/Shotgun.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class Shotgun : MonoBehaviour
 {
-    public ParticleSystem muzzleflash;
-    public ShotgunRecoil recoil; // Referenz zum Recoil Script
+    public int pelletCount = 8;
+    public float spread = 0.1f;
+    public float range = 30f;
+    public int damagePerPellet = 10;
+    public Camera cam;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
+            Shoot();
+    }
+
+    void Shoot()
+    {
+        for (int i = 0; i < pelletCount; i++)
         {
-            muzzleflash.Play();
-            recoil.ApplyRecoil(); // Rückstoß auslösen
+            Vector3 direction = cam.transform.forward;
+            direction.x += Random.Range(-spread, spread);
+            direction.y += Random.Range(-spread, spread);
+
+            if (Physics.Raycast(cam.transform.position, direction, out RaycastHit hit, range))
+            {
+                EnemyHealth enemy = hit.collider.GetComponent<EnemyHealth>();
+                if (enemy != null)
+                    enemy.TakeDamage(damagePerPellet);
+            }
         }
     }
 }
